376ecc6263eaf2e01e14c425af82674ca3d10e9d
xiejianjun <963711442@qq.com>
2019-05-05 11:29:34 +0800

    将parcel加入，把button改成单文件组件

--- button/button.js
+++
@@ -1,5 +0,0 @@
-Vue.component('nt-button',{
-    template: `
-    <button class="nt-button">按钮</button>
-    `
-})
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Button from './button'
+
+Vue.component('nt-button',Button)
+new Vue({
+    el: '#app'
+})
