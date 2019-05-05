a933b0b5d75bba4ec6d17976687d7506bfbf7d59
xiejianjun <963711442@qq.com>
2019-05-05 16:51:15 +0800

    添加icon和完善Icon样式

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,16 @@
 <template>
-    <button class="nt-button">按钮</button>
+    <button class="nt-button" :class=`icon-${iconPosition}`>
+        <svg v-if="iconName" class="icon">
+            <use :xlink:href=`#i-${iconName}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
 export default {
+    props: ['icon-name', 'icon-position']
 }
 </script>
 <style lang="scss">
@@ -13,14 +21,18 @@ export default {
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-        &:hover {
-            border-color:var(--border-color-hover)
-        }
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-        &:focus{
-            outline: none;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: top;
+        &:hover { border-color: var(--border-color-hover) }
+        &:active { background-color: var(--button-active-bg); }
+        &:focus { outline: none; }
+        > .icon { order: 1; margin-right: .3em }
+        > .content{ order: 2; }
+        &.icon-right {
+            > .icon { order: 2; margin-right: 0; margin-left: .3em }
+            > .content { order: 1; }
         }
     }
 </style>
