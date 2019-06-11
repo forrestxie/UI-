da6cceae8cbf80db81630f09dea57f442bb90248
xiejianjun <963711442@qq.com>
2019-06-11 17:10:43 +0800

    系统布局完成

---
+++ src/layout.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="layout" :class="layoutClass">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Gulu-layout',
+    data(){
+        return {
+            layoutClass: {
+                hasSider: false
+            }
+        }
+    },
+    mounted() {
+        this.$children.forEach((vm)=>{
+            console.log(vm.$options.name, this.layoutClass)
+            if(vm.$options.name === 'Gulu-sider') {
+                this.layoutClass.hasSider = true
+            }
+        })
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .layout {
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        border: 1px solid red;
+        &.hasSider{
+            flex-direction: row;
+        }
+    }
+</style>
---
+++ src/sider.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="sider">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Gulu-sider'
+}
+</script>
+<style lang="scss">
+    .sider {
+        border: 1px solid red;
+    }
+</style>
