d8f5706ac2164d697056943c2df3ef9172a5b089
xiejianjun <963711442@qq.com>
2019-06-16 20:04:46 +0800

    解决容器overflow:hidden的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,11 +1,12 @@
+<!--这个轮子不支持overflow：hidden,需要下方的按钮和content分离，故要重写-->
 <template>
-    <div class="popover">
-        <div @click.stop class="content-wrapper" v-if="visible">
+    <div class="popover" @click.stop="xxx">
+        <div  ref="contentWrapper" @click.stop class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <div @click.stop="xxx">
+        <span ref="triggerWrapper">
             <slot></slot>
-        </div>
+        </span>
     </div>
 </template>
 <script>
@@ -24,12 +25,20 @@ export default {
         xxx() {
             this.visible = !this.visible
             if(this.visible === true) {
-               let clickDocument = ()=> {
-                   console.log('监听器添加')
-                    this.visible = false
-                    document.removeEventListener('click', clickDocument)
-                }
-                document.addEventListener('click', clickDocument)
+                this.$nextTick(()=> {
+                    document.body.appendChild(this.$refs.contentWrapper)
+                    let {left, top, height, width} = this.$refs.triggerWrapper.getBoundingClientRect()
+                    console.log(left, top, height, width)
+                    this.$refs.contentWrapper.style.left = left+ window.scrollX + 'px'
+                    this.$refs.contentWrapper.style.top = `${top-height+window.scrollY}px`
+                    this.$refs.contentWrapper.style.position = 'absolute'
+                    let clickDocument = ()=> {
+                        console.log('监听器添加')
+                        this.visible = false
+                        document.removeEventListener('click', clickDocument)
+                    }
+                    document.addEventListener('click', clickDocument)
+                })
             }
         }
     },
---
+++ src/realPopover.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+</template>
+<script>
+export default {
+    mixins: [],
+    props: [],
+    data() {
+        return {}
+    },
+    computed: {},
+    created() {
+    },
+    mounted() {
+    },
+    methods: {},
+    watch: {}
+}
+</script>
+<style lang="stylus" module>
+</style>
